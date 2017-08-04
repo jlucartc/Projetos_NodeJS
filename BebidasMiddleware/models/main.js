5d991d9172f804574e7e638759260221df3d4cf2
jlucartc <jlucartc@gmail.com>
2017-08-04 18:20:32 -0300

    201708041820

--- BebidasMiddleware/models/main.js
+++ BebidasMiddleware/models/main.js
@@ -1,12 +1,43 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test');
-
 var Schema = mongoose.Schema;
 
-var db = mongoose.connection;
+var lojasSchema = new Schema({
+  nome : String,
+  empresa_id : Schema.ObjectID,
+  end : {
+    bairro : String,
+    cep : String,
+    rua : String,
+    numero : Number,
+    predio : String,
+    apartamento : String
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("we're connected!");
+  },
+  produtos : [{ id : Schema.ObjectID }]
+});
+
+var produtosSchema = new Schema({
+  nome : String,
+  marca : String,
+  volume : { vLitro : Number, vmL : Number}
+});
+
+var mapaSchema = new Schema();
+
+var pedidosSchema = new Schema({
+  codigo : String,
+  nome : String,
+  modoEntrega : Boolean,
+  data : Date,
+  telefone : String,
+  email : String,
+  end : {
+    cep : Number,
+    numero : Number,
+    bloco : String,
+    apartamento : String
+  }
+  listaProdutos : [{ id : Schema.ObjectID , quantidade : Number, valor : Number}]
+  valor : Number
 });

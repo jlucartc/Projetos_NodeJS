1338932064bc661d7db0e3e7e7e1c4dc3af8eeb2
jlucartc <jlucartc@gmail.com>
2017-08-04 18:46:10 -0300

    201708041846

--- BebidasMiddleware/models/main.js
+++ BebidasMiddleware/models/main.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var lojasSchema = new Schema({
+var lojaSchema = new Schema({
   nome : String,
   empresa_id : Schema.ObjectID,
   end : {
@@ -17,7 +17,7 @@ var lojasSchema = new Schema({
   produtos : [{ id : Schema.ObjectID }]
 });
 
-var produtosSchema = new Schema({
+var produtoSchema = new Schema({
   nome : String,
   marca : String,
   volume : { vLitro : Number, vmL : Number}
@@ -25,7 +25,7 @@ var produtosSchema = new Schema({
 
 var mapaSchema = new Schema();
 
-var pedidosSchema = new Schema({
+var pedidoSchema = new Schema({
   codigo : String,
   nome : String,
   modoEntrega : Boolean,
@@ -41,3 +41,11 @@ var pedidosSchema = new Schema({
   listaProdutos : [{ id : Schema.ObjectID , quantidade : Number, valor : Number}]
   valor : Number
 });
+
+var lojaModel = mongoose.model('Lojas', lojaSchema);
+var produtoModel = mongoose.model('Produtos', produtoSchema);
+var pedidoModel = mongoose.model('Pedidos', pedidoSchema);
+
+exports.Loja = lojaModel;
+exports.Produto = produtoModel;
+exports.Pedido = pedidoModel;
--- BebidasMiddleware/routes/lojaPerfil.js
+++ BebidasMiddleware/routes/lojaPerfil.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var Lojas = require('./../models/main.js');
+
+var loja = new Lojas();
+
+var listaLojas = null;
+
+loja.find(function(err,lojas){
+  if(err) throw err;
+  listaLojas = lojas;
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('lojaPerfil', { title: 'Express' , lojas : listaLojas});
 });
 
 module.exports = router;
